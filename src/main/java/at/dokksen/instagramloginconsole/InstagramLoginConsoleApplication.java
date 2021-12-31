02e92709b8ea2829dcec83bf3dd11e16b46218cc
Dogan Sahin <dsa5439@students.fhv.at>
2021-12-31 16:05:48 +0100

    adding files

---
+++ src/main/java/at/dokksen/instagramloginconsole/InstagramLoginConsoleApplication.java
@@ -0,0 +1,13 @@
+package at.dokksen.instagramloginconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramLoginConsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstagramLoginConsoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/dokksen/instagramloginconsole/InstagramLoginConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package at.dokksen.instagramloginconsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramLoginConsoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
